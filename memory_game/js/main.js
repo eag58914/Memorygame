42ac213a4138bc596cfa0065c74de81c50853309
Elco Garcia <garciaelco@gmail.com>
2019-05-21 16:10:33 -0500

    made final changes

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,6 @@
 
-cards=[
-    {
+ var cards=
+   [ {
     rank: "queen",
     suit: "hearts",
     cardImage: "images/queen-of-hearts.png"
@@ -19,9 +19,8 @@ cards=[
     rank: "king",
     suit: "diamonds",
     cardImage:"images/king-of-diamonds.png"
-    }  
-];
-cardsInPlay=[];
+    } ];
+var cardsInPlay=[];
 
 function checkForMatch(){
     if(cardsInPlay[0]=== cardsInPlay[1]){
@@ -31,14 +30,26 @@ function checkForMatch(){
     }
 };
 
-function flipCard(cardId){
-    cardsInPlay.push(cards[cardId].rank);
-console.log("User flipped " + cards[cardId].rank);
-console.log(cards[cardId].suit);
-console.log(cards[cardId].cardImage);
-if(cardsInPlay.length ===2){
+function flipCard(){
+ const cardId = this.getAttribute('data-id');
+ const card =cards[cardId];
+this.setAttribute('src',card.cardImage);
+console.log('User Flipped ' + cards[cardId].rank);
+console.log(card.suit);
+console.log(card.cardImage);
+cardsInPlay.push(card.rank);
+if(cardsInPlay.length === 2){
 checkForMatch();
 }
 }
-flipCard(0);
-flipCard(1);
+
+function createBoard(){
+    for (i = 0; i < cards.length; i ++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute("src",'images/back.png');
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+   var board = document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+createBoard();
